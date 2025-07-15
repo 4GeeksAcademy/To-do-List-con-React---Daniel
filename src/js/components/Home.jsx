57bddd9f7252e936e2e7f6e098ca074459ca47db
Dan <danielgarv2800@gmail.com>
2025-07-15 00:01:55 +0000

    Agregando fetch a mi To Do List

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Tarea from "./Tarea";
 
 
 const Home = () => {
-	let [NuevaTarea, setNuevaTarea] = useState("");  
-	let [ListaDeTareas, setListaDeTareas] = useState([
-		"Practice Js",
-		"Practice React",
-		"Do some exercice",
-		"Read",
-		"Talk with TLOML",
-	]);
+
+	const ApiUrl = "https://playground.4geeks.com/todo/users/Dan28"
+
+	let [NuevaTarea, setNuevaTarea] = useState("");
+
+	let [ListaDeTareas, setListaDeTareas] = useState([]);
+
+	const onLoad = () => {
+		fetch(ApiUrl).then(response => {
+			return response.json()
+		}).then(datos => {
+			setListaDeTareas(datos.todos)
+		})
+	}
+
+	useEffect(onLoad, []);
 
 	const AgregarTarea = (key) => {
 		if (key === "Enter" && NuevaTarea.trim() !== "") {
-			setListaDeTareas([...ListaDeTareas, NuevaTarea.trim()]);
-			setNuevaTarea(""); 
+			//setListaDeTareas([...ListaDeTareas, NuevaTarea.trim()]);
+			
+
+			fetch('https://playground.4geeks.com/todo/todos/Dan28', {
+      method: "POST",
+      body: JSON.stringify({
+		label: NuevaTarea,
+		is_done: false
+	  }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); 
+		if (resp.ok){
+			//onLoad()
+			setNuevaTarea("");
+		}
+        console.log(resp.status); 
+        return resp.json(); 
+    })
+    .then(data => {
+		
+        console.log(data); 
+		setListaDeTareas([...ListaDeTareas, datos]);
+    })
+    .catch(error => {
+        
+        console.log(error);
+    });
 		}
 	};
 
-	const BorrarTarea = (index)  => {
-		setListaDeTareas(ListaDeTareas.filter((item, i) => index != i ))
+	const BorrarTarea = (index) => {
+		setListaDeTareas(ListaDeTareas.filter((item, i) => index != i))
 	};
 
 	return (
@@ -32,11 +69,11 @@ const Home = () => {
 					type="text"
 					value={NuevaTarea}
 					onKeyUp={event => AgregarTarea(event.key)}
-				/>				
+				/>
 			</div>
 			{
 				ListaDeTareas.map((tarea, index) => {
-					return (<Tarea key={index} descripcion={tarea} onDelete={() => BorrarTarea(index)} />) 
+					return (<Tarea key={index} descripcion={tarea.label} onDelete={() => BorrarTarea(index)} />)
 				})
 			}
 		</div>
